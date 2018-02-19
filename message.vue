c7294866c9833904d1c48f3aef2b6b08cb65b12d
oz <smechailes@gmail.com>
2018-02-19 23:55:55 +0545

    Displaying message with vue files

---
+++ message.vue
@@ -0,0 +1,17 @@
+<template>
+	<p class="box">
+		<slot></slot>
+	</p>
+</template>
+
+<script>
+	export default{
+
+	}
+</script>
+
+<style lang="scss">
+	.box{
+		background-color: orange;
+	}
+</style>
